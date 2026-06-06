397b969f0674374ade22631226604fa07a6ec78e
justyoonjh <juhoyoon0901@naver.com>
2026-06-07 01:54:33 +0900

    USART driver settings-4 1. USART driver complete SetBaudData(USARTDIV, Mantissa and fraction) 2. 015uart_tx.c frame

---
+++ stm32f4xx_drivers/Src/015uart_tx.c
@@ -0,0 +1,34 @@
+/*
+ * 015uart_tx.c
+ *
+ *  Created on: 2026. 6. 6.
+ *      Author: justjhst
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "stm32f446xx.h"
+
+char msg[1024] = "UART Tx testing...\n\r";
+
+USART_Handle_t usart2_handle;
+
+void USART1_Init(void)
+{
+
+}
+
+void USART1_GPIOInit(void)
+{
+
+}
+
+void GPIO_ButtonInit(void)
+{
+
+}
+
+void delay(void)
+{
+
+}
